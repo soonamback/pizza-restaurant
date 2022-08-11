3e2adcdecc450ba51ef9e2ea961c9cf6357f81a2
Soonam Yi <soonamyi@mbp-von-soonam.speedport_w_724v_09011603_06_010>
2022-08-11 10:43:24 +0200

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,18 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+<p>hello</p>
+<Header />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+   Header
+  },
 }
 </script>
 
